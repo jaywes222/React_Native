fed6a5e2186d5d92bad4479854a3140a8dad4a5c
Jay Wes <wesleyjay202@gmail.com>
2024-10-11 12:43:17 +0300

    Text Input

---
+++ Mini-Game/screens/StartGameScreen.js
@@ -0,0 +1,49 @@
+import { StyleSheet, TextInput, View } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen() {
+	return (
+		<>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.numberInput}
+					maxLength={'2'}
+					keyboardType="number-pad"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+				/>
+				<PrimaryButton>Reset</PrimaryButton>
+				<PrimaryButton>Confirm</PrimaryButton>
+			</View>
+		</>
+	);
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		padding: 16,
+		marginTop: 100,
+		marginHorizontal: 24,
+		borderRadius: 8,
+		backgroundColor: '#72063c',
+		elevation: 4, // Android
+		//iOS
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.5,
+	},
+	numberInput: {
+		height: 50,
+		width: 50,
+		fontSize: 32,
+		borderBottomColor: '#ddb52f',
+		borderBottomWidth: 2,
+		color: '#ddb52f',
+		marginVertical: 8,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+});
