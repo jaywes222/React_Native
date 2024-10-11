b12ff8d0367a17d0279fc231e96618b7b06fda61
Jay Wes <wesleyjay202@gmail.com>
2024-10-11 16:17:28 +0300

    Checking fo "Game Over"

--- Mini-Game/App.js
+++ Mini-Game/App.js
@@ -4,18 +4,29 @@ import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 import Colors from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
 	const [userNumber, setUserNumber] = useState();
+	const [gameIsOver, setGameIsOver] = useState(true);
 
 	function pickedNumberHandler(pickedNumber) {
 		setUserNumber(pickedNumber);
+		setGameIsOver(false);
+	}
+
+	function gameOverHandler() {
+		setGameIsOver(true);
 	}
 
 	let screen = <StartGameScreen onPickedNumber={pickedNumberHandler} />;
 
 	if (userNumber) {
-		screen = <GameScreen userNumber={userNumber} />;
+		screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
+	}
+
+	if (gameIsOver && userNumber) {
+		screen = <GameOverScreen />;
 	}
 
 	return (
--- Mini-Game/screens/GameScreen.js
+++ Mini-Game/screens/GameScreen.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
@@ -17,13 +17,19 @@ function generateRandomBetween(min, max, exclude) {
 let minBoundary = 1;
 let maxBoundary = 100;
 
-function GameScreen({ userNumber }) {
+function GameScreen({ userNumber, onGameOver}) {
 	const initialGuess = generateRandomBetween(
-		minBoundary,
-		maxBoundary,
+		1,
+		100,
 		userNumber
 	);
-	const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    
+    useEffect(() => {
+        if (currentGuess === userNumber) {
+            onGameOver();
+        }
+    }, [currentGuess, userNumber, onGameOver])
 
 	function nextGuessHandler(direction) {
 		if (
