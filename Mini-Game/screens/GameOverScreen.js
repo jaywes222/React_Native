2eaaa46f51907e476c677bc9b38f591e949c11c5
Jay Wes <wesleyjay202@gmail.com>
2024-10-12 23:30:33 +0300

    Game Over Screen

--- Mini-Game/screens/GameOverScreen.js
+++ Mini-Game/screens/GameOverScreen.js
@@ -1,17 +1,24 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
 import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
+import PrimaryButton from './../components/ui/PrimaryButton';
 
 function GameOverScreen() {
 	return (
 		<View style={styles.rootContainer}>
 			<Title>GAME OVER!</Title>
 			<View style={styles.imageContainer}>
-				<Image style={styles.image} source={require('../assets/images/success.png')} />
-            </View>
-            <View>
-                <Text>Your Phone needed X Round(s) to Guess the Number Y.</Text>
-            </View>
+				<Image
+					style={styles.image}
+					source={require('../assets/images/success.png')}
+				/>
+			</View>
+			<View>
+				<Text style={styles.summaryText}>
+					Your Phone needed <Text style={styles.highlight}>X</Text> Round(s) to Guess the Number <Text style={styles.highlight}>Y</Text>.
+                </Text>
+                <PrimaryButton>Start New Game</PrimaryButton>
+			</View>
 		</View>
 	);
 }
@@ -19,12 +26,12 @@ function GameOverScreen() {
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-    rootContainer: {
-        flex: 1,
-        padding: 24,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+	rootContainer: {
+		flex: 1,
+		padding: 24,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 	imageContainer: {
 		width: 300,
 		height: 300,
@@ -33,9 +40,19 @@ const styles = StyleSheet.create({
 		borderColor: Colors.primary800,
 		overflow: 'hidden',
 		margin: 36,
-    },
-    image: {
-        width: '100%',
-        height: '100%'
-    }
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+	},
+	summaryText: {
+        fontFamily: 'open-sans',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24
+	},
+	highlight: {
+		fontFamily: 'open-sans-bold',
+		color: Colors.primary500,
+	},
 });
