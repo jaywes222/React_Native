46cf5bcaa0b91ba486660fb7016e6c863a2993e4
Jay Wes <wesleyjay202@gmail.com>
2024-10-13 18:13:34 +0300

    Categories Screen

--- Meals_App/screens/CategoriesScreen.js
+++ Meals_App/screens/CategoriesScreen.js
@@ -1,5 +1,24 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from './../data/dummy-data';
+
+function renderCategoryItem(itemData) {
+	return (
+		<CategoryGridTile
+			title={itemData.item.title}
+			color={itemData.item.color}
+		/>
+	);
+}
+
 function CategoriesScreen() {
-    return 
-};
+	return (
+		<FlatList
+			data={CATEGORIES}
+			keyExtractor={(item) => item.id}
+			renderItem={renderCategoryItem}
+		/>
+	);
+}
 
 export default CategoriesScreen;
